a64d7fbe4354a5e1e3ac9dfd10a59f85c1ad0c53
Moses, Oluwadamilare Joshua <mjoshua241678@stu.ui.edu.ng>
2026-06-14 22:58:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,6 @@ import json
 import requests  # NEW: This library allows Python to talk to Google
 from datetime import datetime
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"], 
-    allow_credentials=True,
-    allow_methods=["*"], 
-    allow_headers=["*"], 
-)
-
 app = FastAPI()
 
 app.add_middleware(
